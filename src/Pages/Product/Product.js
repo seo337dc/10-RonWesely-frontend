8f23de25910694e09df48fc06122f7f4db4aa721
서동찬 <sdc337dc@naver.com>
2020-07-28 11:58:25 +0900

    [서동찬] Modify : 위즐리 프로젝트작업 복구 (대용량 패키시 + 제품보기 + 각 컴포넌트 위치 변동, + 컴포넌트 분리 및 css 분리 + 페이지 이동 정리) - 대용량 패키지 (BulkPackageSale) : 컴포넌트 위치 Page로 이동, 각 컴포넌트 css 추가 - 제품보기 및 사이드바 (Product, Sidebar) : 네비게이션 추가 및 css 정리 - Nav : Nav, NavSub, NavProduct 위치 및 css 정리 - Router 추가 : 각 페이지에 이동이 필요한 부분은 router 추가

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -1,59 +1,55 @@
 import React, { Component } from "react";
-import Nav from "./Nav/Nav";
+import Nav from "../../Component/Nav/Nav";
 import SetPresent from "./ProductList/SetPresent";
-import SideBar from "../../Component/SideBar";
+import SideBar from "../../Component/SideBar/SideBar";
 import "./Product.scss";
 
 class Product extends Component {
   constructor() {
     super();
+
     this.state = {
       sideBarValid: false,
-      list: [
-        {
-          title: "선물세트",
-          titleSub: "면도용품+기프트 카드",
-          price: "8,900원",
-        },
-      ],
     };
   }
+
   changeSideBarValidInSideBar = (check) => {
-    console.log(check);
     this.setState({
       sideBarValid: !check,
     });
   };
+
   changeSideBarValidInProduct = () => {
-    console.log();
     this.setState({
-      sideBarValid: !this.state.changeSideBarValid,
+      sideBarValid: !this.state.sideBarValid,
     });
   };
+
   render() {
-    const position = { position: "fixed" };
+    const index = !this.props.location.state
+      ? 1
+      : this.props.location.state.index;
+
     return (
       <>
+        <div className={this.state.sideBarValid ? "Product-fixed" : ""}></div>
         <SideBar
           sideBarValid={this.state.sideBarValid}
           changeSideBarValid={this.changeSideBarValidInSideBar}
         />
-        <div
-          className={
-            this.state.sideBarValid ? "Product-fix" : "Product-relative"
-          }
-        >
-          {this.state.list.map((data) => (
-            <Nav
-              key={data.title}
-              data={data}
-              changeSideBarValid={this.changeSideBarValidInProduct}
-            />
-          ))}
 
-          <SetPresent />
+        <Nav
+          activeProductNav={index}
+          changeSideBarValidInProduct={this.changeSideBarValidInProduct}
+        />
+
+        <div className={this.state.sideBarValid ? "Product-none" : "Product"}>
+          {index === 1 && <SetPresent />}
+          {index === 2 && <div></div>}
+          {index === 3 && <div></div>}
+          {index === 4 && <div></div>}
+          {index === 5 && <div></div>}
         </div>
-        {this.state.sideBarValid ? <div className="Product-none"></div> : null}
       </>
     );
   }
--- src/Pages/Product/ProductList/SetPresent.js
+++ src/Pages/Product/ProductList/SetPresent.js
@@ -3,29 +3,24 @@ import "./SetPresent.scss";
 
 class SetPresent extends Component {
   state = {
-    newKey: 1,
+    newKey: 0,
   };
+
   handleChangeNew = (id) => {
     this.setState({
       newKey: id,
     });
   };
+
   render() {
-    let new_value = "";
-    switch (this.state.newKey) {
-      case 1:
-        new_value =
-          "독점화된 면도기 시장을 '가성비'로 뚫은 브랜드, 시장을 정확히파고 들었다.";
-        break;
-      case 2:
-        new_value =
-          "다이아몬드 코팅된 독일산5중 날로서 우수한 절삭력을 보여주는데도 1만원 이하다.";
-        break;
-      case 3:
-        new_value =
-          "뛰어난 절삭력, 간결한 구성과 효율적인 디자인이 만들어내는만족감";
-        break;
-    }
+    const { newKey } = this.state;
+    const newArr = [
+      "독점화된 면도기 시장을 '가성비'로 뚫은 브랜드, 시장을 정확히파고 들었다.",
+      "다이아몬드 코팅된 독일산5중 날로서 우수한 절삭력을 보여주는데도 1만원 이하다.",
+      "뛰어난 절삭력, 간결한 구성과 효율적인 디자인이 만들어내는만족감",
+    ];
+    let new_value = newArr[newKey];
+
     return (
       <div className="SetPresent">
         <section className="container">
@@ -73,15 +68,15 @@ class SetPresent extends Component {
               </div>
               <div className="new-icon">
                 <img
-                  onClick={() => this.handleChangeNew(1)}
+                  onClick={() => this.handleChangeNew(0)}
                   src="https://wiselyshave-cdn.s3.amazonaws.com/assets/images/news/news_1.svg"
                 />
                 <img
-                  onClick={() => this.handleChangeNew(2)}
+                  onClick={() => this.handleChangeNew(1)}
                   src="https://wiselyshave-cdn.s3.amazonaws.com/assets/images/news/news_2.svg"
                 />
                 <img
-                  onClick={() => this.handleChangeNew(3)}
+                  onClick={() => this.handleChangeNew(2)}
                   src="https://wiselyshave-cdn.s3.amazonaws.com/assets/images/news/news_3.svg"
                 />
               </div>
