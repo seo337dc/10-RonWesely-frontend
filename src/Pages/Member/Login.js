3deee8aaf4e9038907bef16e32cd1c5ea22d4606
leeyosi <dark235612@gmail.com>
2020-07-29 22:15:44 +0900

    [이윤식] Refactor : 로그인 페이지 리팩토링

--- src/Pages/Member/Login.js
+++ src/Pages/Member/Login.js
@@ -6,7 +6,7 @@ class Login extends Component {
   constructor() {
     super();
     this.state = {
-      userid: "",
+      email: "",
       password: "",
       displayActive: false,
     };
@@ -16,11 +16,11 @@ class Login extends Component {
     this.props.history.push("/signup");
   };
 
-  handlelogin = (e) => {
-    fetch("http://10.58.6.255:8000/users/SignIn", {
+  handleClick = (e) => {
+    fetch("http://10.58.2.20:8000/user/SignIn", {
       method: "POST",
       body: JSON.stringify({
-        userid: this.state.userid,
+        email: this.state.email,
         password: this.state.password,
       }),
     })
@@ -28,7 +28,7 @@ class Login extends Component {
       .then((res) => {
         console.log(res);
         localStorage.setItem("access_token", res.access_token);
-        this.props.history.push("/main");
+        this.props.history.push("/mypage");
       });
   };
 
@@ -43,8 +43,10 @@ class Login extends Component {
   };
 
   render() {
-    const { userid, password } = this.state;
-
+    const { email, password } = this.state;
+    const idvalid =
+      email.includes("@") && email.includes(".co") && email.length >= 10;
+    console.log(this.state);
     return (
       <div className="Login">
         <img
@@ -89,69 +91,50 @@ class Login extends Component {
               />
             </div>
           </div>
-          <div
-            className={
-              this.state.displayActive ? "email-box-visible" : "email-box-none"
-            }
-          >
+          <div className={this.state.displayActive ? "email-box" : "none"}>
             <div
               className={
-                userid.includes("@") &&
-                userid.includes(".co") &&
-                userid.length >= 10
-                  ? "email-input-container-blue"
-                  : "email-input-container"
+                idvalid ? "email-input-container blue" : "email-input-container"
               }
             >
               <input
                 type="text"
                 placeholder="이메일"
                 className="email-input"
-                name="userid"
+                name="email"
                 onChange={this.inputHandler}
               />
               <img
-                className={
-                  userid.includes("@") &&
-                  userid.includes(".co") &&
-                  userid.length >= 10
-                    ? "check-img-visible"
-                    : "check-img-none"
-                }
+                className={idvalid ? "check-img-visible" : "none"}
                 src="https://wiselyshave-cdn.s3.amazonaws.com/assets/images/checkBlue.svg"
               />
             </div>
             <div
               className={
                 password.length >= 6
-                  ? "pw-input-container-blue"
+                  ? "pw-input-container blue"
                   : "pw-input-container"
               }
             >
               <input
-                type="text"
+                type="password"
                 placeholder="비밀번호 (6자 이상)"
                 className="pw-input"
                 name="password"
                 onChange={this.inputHandler}
               />
               <img
-                className={
-                  password.length >= 6 ? "check-img-visible" : "check-img-none"
-                }
+                className={password.length >= 6 ? "check-img-visible" : "none"}
                 src="https://wiselyshave-cdn.s3.amazonaws.com/assets/images/checkBlue.svg"
               />
             </div>
             <button
               className={
-                userid.includes("@") &&
-                userid.includes(".co") &&
-                userid.length >= 10 &&
-                password.length >= 6
-                  ? "login-btn-container-blue"
+                idvalid && password.length >= 6
+                  ? "login-btn-container bg-blue"
                   : "login-btn-container"
               }
-              onClick={this.handlelogin}
+              onClick={this.handleClick}
             >
               <p className="login-btn-text">로그인</p>
             </button>
