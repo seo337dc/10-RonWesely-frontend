19ab61499516a62a64a476683e76c22139fa0bb7
leeyosi <dark235612@gmail.com>
2020-07-30 22:47:49 +0900

    [이윤식] add :  후기 페이지 추가

---
+++ src/Component/Review/Reviewpagination.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Reviewitem from "./reviewitem";
+import "./Reviewpagination.scss";
+
+class Reviewpagination extends Component {
+  constructor() {
+    super();
+    this.state = {
+      review_result: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://10.58.2.54:8000/order/Review")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        this.setState({ review_result: res.review_result });
+      });
+  }
+
+  render() {
+    const { review_result } = this.state;
+    return (
+      <>
+        <div className="show-review-content">
+          {review_result.map((item) => {
+            return (
+              <Reviewitem
+                rate={item.rate}
+                name={item.name}
+                ages={item.ages}
+                writed_at={item.writed_at}
+                review_text={item.review_text}
+              />
+            );
+          })}
+        </div>
+      </>
+    );
+  }
+}
+
+export default Reviewpagination;
